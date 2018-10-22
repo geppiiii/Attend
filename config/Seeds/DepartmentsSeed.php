8d4e396656dd90d96773942dd90f16c11b4d8774
和田海斗 <kaito@diffeasy.com>
2018-10-22 10:39:14 +0900

    Seed追加

--- config/Seeds/DepartmentsSeed.php
+++ config/Seeds/DepartmentsSeed.php
@@ -69,6 +69,45 @@ class DepartmentsSeed extends AbstractSeed
             ],
             [
                 'department_name' => '国際ビジネス科'
+            ],
+            [
+                'department_name' => 'エアライン科'
+            ],
+            [
+                'department_name' => 'エアポート科'
+            ],
+            [
+                'department_name' => 'ブライダル・ウエディング科'
+            ],
+            [
+                'department_name' => 'トラベル科'
+            ],
+            [
+                'department_name' => 'ホテル・リゾート科'
+            ],
+            [
+                'department_name' => '海外ビジネス科'
+            ],
+            [
+                'department_name' => '英語コミュニケーション科'
+            ],
+            [
+                'department_name' => '製菓パティシエ科'
+            ],
+            [
+                'department_name' => '医療秘書・事務科'
+            ],
+            [
+                'department_name' => '診療情報管理士専攻科'
+            ],
+            [
+                'department_name' => '診療情報管理士科'
+            ],
+            [
+                'department_name' => 'こども未来学科'
+            ],
+            [
+                'department_name' => '社会福祉科'
             ]
 
         ];
