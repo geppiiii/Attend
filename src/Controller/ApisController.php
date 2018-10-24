57f55ad8d73014268b7dc64495c107ba4a1b40a2
和田海斗 <kaito@diffeasy.com>
2018-10-24 14:53:35 +0900

    途中コミット

--- src/Controller/ApisController.php
+++ src/Controller/ApisController.php
@@ -25,48 +25,22 @@ class ApisController extends AppController {
     }
 
 
-    public function attends_HR(){
+    public function add(){
+        $request_code = "BAD_REQUEST_CODE";
+        $request_number = '404';
         $this->autoRender = false;
-        $data = ["BAD_REQUEST_CODE" => '404'];
+        $data = [$request_code => $request_number];
         $attendsTable = TableRegistry::get('attends');
         $studentsTable = TableRegistry::get('students');
 
         if($this->request->is('post')){
-            $number = $articles->get('ic_number');
-            $break = $articles->get('break');
-            $student_est = $studentsTable->get($number);
-            try{
-                $attends_est = $attendsTable->get($student_est->student_number);
-            } catch(\Exception $e) {
-                $attends_est = null;
-            }
+            $request_code = "NORMAL_REQUEST_CODE";
+            $request = json_decode(file_get_contents('php://input'), true);
+            $request_number = $request['id'];
+            // $request_number = $_POST['id'];
 
-            if($break == true && !empty($attends_est)){ 
-                //現在時刻に適応した時刻をいれる。
-                $attends_est->leave_time = date("H/i/s");
-                //帰宅情報を入れる。
-                
-                //if文で、条件文をかいて、早退処理をかく
-                if($attends_est->attend_state = 6 && $attends_est->leave_state = 1){
-                    $attends_est->all_situation = 2;//遅刻
-                } else {
-                    $attends_est->all_situation = $attends_est->leave_state;//正常
-                }
-                
-                $data = ["GOOD_REQUEST_CODE" => '200'];
-            }else{
-                //朝
-                //出席情報を入れる。
-                $attends_est = array(
-                    'student_number' => h($student_est->student_number),
-                    'attend_time' => h(date("H/i/s"))
-                );
-                $attendsTable = $attends->newEntity($this->request->getData());
-                if ($attends->save($attendsTable)) {
-                    $data = ["GOOD_REQUEST_CODE" => '200'];
-                }
-            }     
-           
+
+            $data = [$request_code => $request_number];
         }
         $this->response->body(json_encode($data));
     }
