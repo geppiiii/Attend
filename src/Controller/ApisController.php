5a9369060448911917bb2066404c54cad0bd1ca9
和田海斗 <kaito@diffeasy.com>
2018-11-05 10:38:15 +0900

    途中コミット

--- src/Controller/ApisController.php
+++ src/Controller/ApisController.php
@@ -28,6 +28,7 @@ class ApisController extends AppController {
     public function add(){
         $request_code = "BAD_REQUEST_CODE";
         $request_number = '404';
+        $student_id = null;
         $this->autoRender = false;
         $data = [$request_code => $request_number];
         $attendsTable = TableRegistry::get('attends');
@@ -36,10 +37,12 @@ class ApisController extends AppController {
         if($this->request->is('post')){
             $request_code = "NORMAL_REQUEST_CODE";
             $request = json_decode(file_get_contents('php://input'), true);
-            $request_number = $request['id'];
-            // $request_number = $_POST['id'];
-
-
+            $ic_number = $request['id'];
+            $students_result = $studentsTable->find()->select(['student_number'])->where(['ic_number'=>$ic_number]);
+            foreach ($students_result as $result) {
+                $student_id = $result->student_number;
+            }
+            $request_number = $student_id;
             $data = [$request_code => $request_number];
         }
         $this->response->body(json_encode($data));
